a6bd68e7c470d109782ef3fd6c56287f5d41a1ea
linhfishCR7 <fish@devx.com.au>
2022-03-28 21:26:14 +0700

    Update salary

--- salaries/hrm/serializers.py
+++ salaries/hrm/serializers.py
@@ -109,7 +109,6 @@ class SalarySerializer(serializers.ModelSerializer):
                 staff_project=staff_project.id,
                 date__month=timezone.now().month-1
             ).values()
-            print_value()
             total = 0
             for overtime_data in overtime:
                 total += overtime_data['amount_time']*overtime_data['type']
@@ -123,7 +122,7 @@ class SalarySerializer(serializers.ModelSerializer):
         extra=validated_data['extra'] if validated_data['extra'] else 0.0
         other_support=validated_data['other_support'] if validated_data['other_support'] else 0.0
         
-        other=validated_data['other'] if validated_data['other'] else 0.0,
+        other=validated_data['other'] if validated_data['other'] else 0.0
 
         basic_salary=int(basic_salary[0])
         coefficient=int(coefficient)
@@ -135,7 +134,7 @@ class SalarySerializer(serializers.ModelSerializer):
         actual_time = total+SalaryContant.STANDARD_TIME-(day_off_year_detail.amount*8) if day_off_year_detail else total+SalaryContant.STANDARD_TIME
 
         # caculate tax
-        total_salary = basic_salary*coefficient+extra+other_support
+        total_salary = basic_salary*coefficient+extra+other_support+other
         salary_allowance = (basic_salary*coefficient+extra)*(SalaryContant.ALLOWANCE)
         print_value(salary_allowance)
         month_salary = total_salary*(actual_time/(SalaryContant.STANDARD_TIME))-salary_allowance
@@ -198,3 +197,106 @@ class RetrieveAndListSalarySerializer(serializers.ModelSerializer):
             "note",
             "staff",
         ]
+
+    
+    def update(self, instance, validated_data):
+        
+        """
+        Get day off of staffs
+        """
+
+        day_off_year = DayOffYears.objects.filter(
+            is_deleted=False,
+            staff=validated_data['staff'],
+            status=True
+        ).first()
+        print_value(day_off_year)
+
+        day_off_year_detail = DayOffYearDetails.objects.filter(
+            day_off_years=day_off_year.id,
+            from_date__month=timezone.now().month-1,
+            to_date__month=timezone.now().month-1,
+            # day_off_types=
+        ).first()
+
+
+        #TODO get coefficient
+
+
+        print_value(day_off_year_detail)
+        # work project by overtime
+
+        staff_project = StaffProject.objects.filter(
+            staff=validated_data['staff'],
+            project__status=2
+        ).first()
+
+        if staff_project:
+            overtime = Timekeeping.objects.filter(
+                staff_project=staff_project.id,
+                date__month=timezone.now().month-1
+            ).values()
+            total = 0
+            for overtime_data in overtime:
+                total += overtime_data['amount_time']*overtime_data['type']
+        else:
+            total = 0
+            
+
+        
+        basic_salary=validated_data['basic_salary'] if validated_data['basic_salary'] else instance.basic_salary,
+        coefficient=validated_data['coefficient'] if validated_data['coefficient'] else instance.coefficient
+        extra=validated_data['extra'] if validated_data['extra'] else instance.extra
+        other_support=validated_data['other_support'] if validated_data['other_support'] else instance.other_support
+        
+        other=validated_data['other'] if validated_data['other'] else instance.other
+        date=validated_data['date'] if validated_data['date'] else instance.date
+        note=validated_data['note'] if validated_data['note'] else instance.note
+
+        basic_salary=int(basic_salary[0])
+        coefficient=int(coefficient)
+        extra=int(extra) 
+        other_support=int(other_support)
+        other=int(other[0])
+        # overtime=int(overtime[0])
+
+        actual_time = total+SalaryContant.STANDARD_TIME-(day_off_year_detail.amount*8) if day_off_year_detail else total+SalaryContant.STANDARD_TIME
+
+        # caculate tax
+        total_salary = basic_salary*coefficient+extra+other_support+other
+        salary_allowance = (basic_salary*coefficient+extra)*(SalaryContant.ALLOWANCE)
+        print_value(salary_allowance)
+        month_salary = total_salary*(actual_time/(SalaryContant.STANDARD_TIME))-salary_allowance
+        print_value(month_salary)
+        
+        if month_salary <= SalaryContant.M5:
+            tax = month_salary * (SalaryContant.M0_M5)
+        elif month_salary > SalaryContant.M5 and month_salary <= SalaryContant.M10:
+            tax = month_salary * (SalaryContant.UP_M5_M10)
+        elif month_salary > SalaryContant.M10 and month_salary <= SalaryContant.M18:
+            tax = month_salary * (SalaryContant.UP_M10_M18)
+        elif month_salary > SalaryContant.M18 and month_salary <= SalaryContant.M32:
+            tax = month_salary * (SalaryContant.UP_M18_M32)
+        elif month_salary > SalaryContant.M32 and month_salary <= SalaryContant.M52:
+            tax = month_salary * (SalaryContant.UP_M32_M52)
+        elif month_salary > SalaryContant.M52 and month_salary <= SalaryContant.M80:
+            tax = month_salary * (SalaryContant.UP_M52_M80)
+        else:
+            tax = month_salary * (SalaryContant.UP_M80)
+
+
+        salary = Salary.objects.filter(id=instance.id).first()
+
+        salary.date=date
+        salary.actual_time=actual_time
+        salary.basic_salary=basic_salary
+        salary.extra=extra
+        salary.coefficient=coefficient
+        salary.tax=tax
+        salary.other_support=other_support
+        salary.overtime=total
+        salary.other=other
+        salary.note=note
+        salary.save()
+
+        return salary
